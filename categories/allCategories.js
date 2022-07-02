5bf471cd51c0a4dda2564cd36f3f34c5296f3de1
lockck <bohdanw@ukr.net>
2022-07-02 22:57:26 +0300

    1

--- categories/allCategories.js
+++ categories/allCategories.js
@@ -1,10 +1,10 @@
 function allCategories() {
     return  `
-    <a href="https://google.com"><img src="/img/ava.png" alt=""><h2>Project</h2></a>
-    <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
-    <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
-    <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
-    <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
+    <a target = "_blank" href="https://axelar.network/"><img src="/img/Axelar.png" alt="Axelar"><h2>Axelar</h2></a>
+    <a target = "_blank" href="https://bit.country/"><img src="/img/BitCountry.png" alt="Bit.Country"><h2>Bit.Country</h2></a>
+    <a target = "_blank" href="https://nymtech.net/"><img src="/img/Nym.png" alt="Nym"><h2>Nym</h2></a>
+    <a target = "_blank" href="https://zeitgeist.pm/"><img src="/img/ZTG.png" alt=""><h2>Zeitgeist</h2></a>
+    <a target = "_blank" href="https://bit.country/"><img src="/img/ava.png" alt=""><h2>Project</h2></a>
     `;
 }
 export default allCategories;

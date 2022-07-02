8e10e1e2ca2f2c270c2c03ee8bb0a694f1177b64
lockck <bohdanw@ukr.net>
2022-07-02 14:58:18 +0300

    1

--- renderFaqPage.js
+++ renderFaqPage.js
@@ -4,7 +4,7 @@ function renderFaqPage() {
     <div class="block">
         <h1>FAQ</h1>
         <div class="cards1">
-            <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
+            <a><img src="img/ava.png" alt=""><h2>Project</h2></a>
             <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
             <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
             <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>

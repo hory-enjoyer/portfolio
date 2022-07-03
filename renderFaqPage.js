d0fe01259f5eec17c5348a2ccc875f3d8ba0be8f
lockck <bohdanw@ukr.net>
2022-07-03 16:20:38 +0300

    1231

--- renderFaqPage.js
+++ renderFaqPage.js
@@ -4,12 +4,12 @@ function renderFaqPage() {
     <div class="block">
         <h1>FAQ</h1>
         <div class="cards1">
-            <a><img src="img/ava.png" alt=""><h2>Project</h2></a>
-            <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
-            <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
-            <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
-            <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
-            <a><img src="/img/ava.png" alt=""><h2>Project</h2></a>
+            <a><img src="" alt=""><h2>What is Crypto?</h2></a>
+            <a><img src="" alt=""><h2>What is WEB3?</h2></a>
+            <a><img src="" alt=""><h2>About Security?</h2></a>
+            <a><img src="" alt=""><h2>What is Wallet?</h2></a>
+            <a><img src="" alt=""><h2>What is Trading?</h2></a>
+            <a><img src="" alt=""><h2>What is ICO?</h2></a>
         </div>       
     </div>
 `;

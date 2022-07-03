7b85ac0be811467be459b246216702eb61db0678
lockck <bohdanw@ukr.net>
2022-07-03 16:02:54 +0300

    213

--- categories/ambaCategories.js
+++ categories/ambaCategories.js
@@ -1,7 +1,7 @@
 function ambaCategories() {
     return  `
-    <a target = "_blank" href="https://www.connext.network/"><img src="/img/Connext.png" alt="Connext"><h2>Connext</h2></a>
-    <a target = "_blank" href="https://primex.finance/"><img src="/img/Primex.png" alt="Primex"><h2>Primex</h2></a>
+    <a target = "_blank" href="https://www.connext.network/"><img src="https://cdn.discordapp.com/attachments/983771088330817566/993137108812316824/Connext.png" alt="Connext"><h2>Connext</h2></a>
+    <a target = "_blank" href="https://primex.finance/"><img src="https://cdn.discordapp.com/attachments/983771088330817566/993137168526622771/Primex.png" alt="Primex"><h2>Primex</h2></a>
     `;
 }
 export default ambaCategories;

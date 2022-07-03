fe20057a07afca6fe0c899ab1072f6e018e0ac2a
lockck <bohdanw@ukr.net>
2022-07-03 15:39:25 +0300

    1

--- categories/allCategories.js
+++ categories/allCategories.js
@@ -1,21 +1,20 @@
 function allCategories() {
     return  `
-    <a target = "_blank" href="https://axelar.network/"><img src="/img/Axelar.png" alt="Axelar"><h2>Axelar</h2></a>
-    <a target = "_blank" href="https://bit.country/"><img src="/img/BitCountry.png" alt="Bit.Country"><h2>Bit.Country</h2></a>
-    <a target = "_blank" href="https://nymtech.net/"><img src="/img/Nym.png" alt="Nym"><h2>Nym</h2></a>
-    <a target = "_blank" href="https://zeitgeist.pm/"><img src="/img/ZTG.png" alt="ZTG"><h2>Zeitgeist</h2></a>
-    <a target = "_blank" href="https://www.connext.network/"><img src="/img/Connext.png" alt="Connext"><h2>Connext</h2></a>
-    <a target = "_blank" href="https://primex.finance/"><img src="/img/Primex.png" alt="Primex"><h2>Primex</h2></a>
-    <a target = "_blank" href="https://www.minima.global/"><img src="/img/Minima.png" alt="Minima"><h2>Minima</h2></a>
-    <a target = "_blank" href="https://www.kyve.network/#/"><img src="/img/Kyve.png" alt="Kyve"><h2>Kyve</h2></a>
-    <a target = "_blank" href="https://subspace.network/"><img src="/img/Subspace.png" alt="Subspace"><h2>Subspace</h2></a>
-    <a target = "_blank" href="https://gno.land/"><img src="/img/Gno.Land.png" alt="Gno.Land"><h2>Gno.Land</h2></a>
-    <a target = "_blank" href="https://ironfish.network/"><img src="/img/Ironfish.png" alt="Ironfish"><h2>Ironfish</h2></a>
-    <a target = "_blank" href="https://starkware.co/starknet/"><img src="/img/StarkNet.png" alt="StarkNet"><h2>StarkNet</h2></a>
-    <a target = "_blank" href="https://sui.io/"><img src="/img/Sui.png" alt="Sui"><h2>Sui</h2></a>
-    <a target = "_blank" href="https://aptoslabs.com/"><img src="/img/Aptos.png" alt="Aptos"><h2>Aptos</h2></a>
-    <a target = "_blank" href="https://archway.io"><img src="/img/Archway.png" alt="Archway"><h2>Archway</h2></a>
-    <a target = "_blank" href="https://archway.io"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Photos_icon_%282020%29.svg/800px-Google_Photos_icon_%282020%29.svg.png" alt="Archway"><h2>Archway</h2></a>
+    <a target = "_blank" href="https://axelar.network/"><img src="https://drive.google.com/file/d/1j2-MFQ5PFxgoFYKP3Sl1IBMg09ZD8x6Y/view?usp=sharing" alt="Axelar"><h2>Axelar</h2></a>
+    <a target = "_blank" href="https://bit.country/"><img src="https://drive.google.com/file/d/1Fa33oSF_HPMOgFGJiCgJ1q7lry4ui0GI/view?usp=sharing" alt="Bit.Country"><h2>Bit.Country</h2></a>
+    <a target = "_blank" href="https://nymtech.net/"><img src="https://drive.google.com/file/d/1ME9L9JxC8zpdOzH863sk-xQ1DhBy85EI/view?usp=sharing" alt="Nym"><h2>Nym</h2></a>
+    <a target = "_blank" href="https://zeitgeist.pm/"><img src="https://drive.google.com/file/d/1GQ3FqKgmGZl5hnzsfH-CgQsZ01LsqbvX/view?usp=sharing" alt="ZTG"><h2>Zeitgeist</h2></a>
+    <a target = "_blank" href="https://www.connext.network/"><img src="https://drive.google.com/file/d/1nsdTo3lQNFXjF0BxTOUbzGq0gmDgpofL/view?usp=sharing" alt="Connext"><h2>Connext</h2></a>
+    <a target = "_blank" href="https://primex.finance/"><img src="https://drive.google.com/file/d/1HC_6Uqa5YU9WFl2m6gCMGE0e0J2VjIrf/view?usp=sharing" alt="Primex"><h2>Primex</h2></a>
+    <a target = "_blank" href="https://www.minima.global/"><img src="https://drive.google.com/file/d/1P9KoU8124ScALCZo3ltENGaJbKgMjRPh/view?usp=sharing" alt="Minima"><h2>Minima</h2></a>
+    <a target = "_blank" href="https://www.kyve.network/#/"><img src="https://drive.google.com/file/d/143UlCbh3eWA93CRG6w94dJQBPd4K4O5Q/view?usp=sharing" alt="Kyve"><h2>Kyve</h2></a>
+    <a target = "_blank" href="https://subspace.network/"><img src="https://drive.google.com/file/d/16rWF95ILRfg1cKuMBXQxF3lltrGbAnii/view?usp=sharing" alt="Subspace"><h2>Subspace</h2></a>
+    <a target = "_blank" href="https://gno.land/"><img src="https://drive.google.com/file/d/1yMhInbzUDBwY6VRKtMdm_qd_utiZOuHK/view?usp=sharing" alt="Gno.Land"><h2>Gno.Land</h2></a>
+    <a target = "_blank" href="https://ironfish.network/"><img src="https://drive.google.com/file/d/1NEB8ZvbxqEYP8s_Hg1ZBz2IqfyWK6hvi/view?usp=sharing" alt="Ironfish"><h2>Ironfish</h2></a>
+    <a target = "_blank" href="https://starkware.co/starknet/"><img src="https://drive.google.com/file/d/19f-PJfmdweopNMTUphHITIq9X70vXTap/view?usp=sharing" alt="StarkNet"><h2>StarkNet</h2></a>
+    <a target = "_blank" href="https://sui.io/"><img src="https://drive.google.com/file/d/19F-_VsQ4ErcdXSPumAvko2vEfhB4NwvN/view?usp=sharing" alt="Sui"><h2>Sui</h2></a>
+    <a target = "_blank" href="https://aptoslabs.com/"><img src="https://drive.google.com/file/d/1aSeskL2vAbVzK0lF3z2qiHqANCNzi3Nn/view?usp=sharing" alt="Aptos"><h2>Aptos</h2></a>
+    <a target = "_blank" href="https://archway.io"><img src="https://drive.google.com/file/d/1lPoWtBH8qVZqyWGCtP0SgZ-ShPv7sKCf/view?usp=sharing" alt="Archway"><h2>Archway</h2></a>
     `;
 }
 export default allCategories;

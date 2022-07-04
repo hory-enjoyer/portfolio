2d519a7de6c30b33fb6525d095b419d35b384ce8
lockck <bohdanw@ukr.net>
2022-07-04 14:55:50 +0300

    new

--- renderFaqPage.js
+++ renderFaqPage.js
@@ -5,7 +5,7 @@ function renderFaqPage() {
         <h1>FAQ</h1>
         <div class="cards1">
             <a href="https://teletype.in/@lockck/whatIsCrypto" target = "_blank"><img src="https://cdn.discordapp.com/attachments/983771088330817566/993419040557113354/SAD.png" alt=""><h2>What is Crypto?</h2><p>I am often asked what kind of crypto to buy. But do they know what crypto is?<br> Ckick on card to find out!</p></a>
-            <a><img src="" alt=""><h2>What is WEB3?</h2></a>
+            <a href="" target = "_blank"><img src="https://cdn.discordapp.com/attachments/983771088330817566/993445059032784957/pngwing.com_1.png" alt=""><h2>What is WEB 3?</h2><p>I <br> Ckick on card to find out!</p></a>
             <a><img src="" alt=""><h2>About Security?</h2></a>
             <a><img src="" alt=""><h2>What is Wallet?</h2></a>
             <a><img src="" alt=""><h2>What is Trading?</h2></a>

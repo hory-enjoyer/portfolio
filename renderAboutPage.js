88becfe15591ce45a78f2c5d5a6e916c42080cbd
lockck <bohdanw@ukr.net>
2022-07-04 08:14:49 +0300

    new

--- renderAboutPage.js
+++ renderAboutPage.js
@@ -15,18 +15,22 @@ function renderAboutPage() {
     <a target="_blank" href="https://github.com/lockck"><i class="fa-brands fa-github"></i></a>
 </div>
 <div class="content3">
-    <h2>About me</h2>
-    Greeting! I am a front-end developer with a focus on WEB3. I have an experience of community after
-    working for 2 years as customer supporter I took communication skills which have allowed me to manage
-    complex crypto projects. I participate as a moderator/ ambassador/ validator in mainnets and testnets of
-    such networks as cosmos, polkadot, as well as other unique blockchains.
+    <h2>Introduction</h2>
+    Greeting!<br>
+    I'm a front-end developer with a focus on WEB 3. I've an experience of community after
+    working for 2 years as customer supporter. I took communication skills which have allowed me to manage
+    complex crypto projects.<br>
+    I participate as a moderator/ ambassador/ validator in mainnets and testnets of
+    such networks as cosmos, polkadot, as well as other unique blockchains.<br>
     You can see my portfolio already here.
 </div>
 <div class="content4">
-    <h2>FAQ</h2>
-    <p>The idea of this section is to make introductory articles that you can rely on at the beginning of your journey, they will be published periodically. I would be very grateful for the feedback that you can leave under my articles.
-    The idea of this section is to make introductory articles that you can rely on at the beginning of your journey, they will be published periodically. I would be very grateful for the feedback that you can leave under my articles. In the second tab, you can see all my contributions, it can be investments, ambassadorship or validation.
-    </p>
+    <h2>Navigation</h2>
+    <p>In this block, we will consider the second and third tabs, for the sake of completeness.<br>
+
+    In the second tab, you can see all the projects (well, at least the majority) that I have contributed to in one way or another. These can be investments, ambassadors, technical tasks from projects. There will be three tabs on the site: All, Ambassador, Testnet - there is no separate tab for investments, since all projects from the testnet category and which have already been released were invested by me.<br>
+    
+    In the third tab you can watch educational articles. As I am busy, I will publish different content here, at the moment I have chosen educational articles.</p>
 </div>
     </div>
 `;

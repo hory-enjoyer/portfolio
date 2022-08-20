11d57ec145d1fcffdc04c721a7d42f53a6e924b5
lockck <bohdanw@ukr.net>
2022-08-21 00:16:50 +0300

    code was refactored

--- render/renderPortPage.js
+++ render/renderPortPage.js
@@ -2,15 +2,15 @@ function renderAboutPage() {
   sessionStorage.setItem('current', 'port');
   return `
   <div id='ident'>
+  <div class='wrapperText'>
+  <h1>My portfolio</h1>
+  <div class="categories">
+      <a id="all" class="cat">All</a>
+      <a id="amba" class="cat">Ambassador's</a>
+      <a id="test" class="cat">TestNet's</a>
+  </div>
+  </div>
     <div id='blockSize' class="block">
-     <div class='wrapperText'>
-        <h1>My portfolio</h1>
-        <div class="categories">
-            <a id="all" class="cat">All</a>
-            <a id="amba" class="cat">Ambassador's</a>
-            <a id="test" class="cat">TestNet's</a>
-        </div>
-        </div>
         <div id='blockCards' class="cards"></div>   
     </div>
     </div>
